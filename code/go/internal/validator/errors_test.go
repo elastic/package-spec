10fb52e67d4be31d0bef9f48460f86efee2d2437
Shaunak Kashyap <ycombinator@gmail.com>
2020-07-30 06:45:24 -0700

    Adding validation error tests

---
+++ code/go/internal/validator/errors_test.go
@@ -0,0 +1,34 @@
+package validator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidationErrorsMultiple(t *testing.T) {
+	ve := ValidationErrors{}
+	ve = append(ve, "error 1")
+	ve = append(ve, "error 2")
+
+	require.Len(t, ve, 2)
+	require.Contains(t, ve.Error(), "found 2 validation errors:")
+	require.Contains(t, ve.Error(), "error 1")
+	require.Contains(t, ve.Error(), "error 2")
+}
+
+func TestValidationErrorsSingle(t *testing.T) {
+	ve := ValidationErrors{}
+	ve = append(ve, "error 1")
+
+	require.Len(t, ve, 1)
+	require.Contains(t, ve.Error(), "found 1 validation error:")
+	require.Contains(t, ve.Error(), "error 1")
+}
+
+func TestValidationErrorsNone(t *testing.T) {
+	ve := ValidationErrors{}
+
+	require.Len(t, ve, 0)
+	require.Contains(t, ve.Error(), "found 0 validation errors")
+}
--- code/go/internal/validator/package.go
+++ code/go/internal/validator/package.go
@@ -2,12 +2,15 @@ package validator
 
 type Package struct {
 	SpecVersion string
-	RootPath string
+	RootPath    string
 }
 
 func NewPackage(pkgRootPath string) (*Package, error) {
+	// TODO: Check that package root path exists and is a folder
+	// TODO: Check that package root path contains manifest file
+
 	// Load package's manifest file and parse spec version
-	specVersion := "1.0.0"
+	specVersion := "1.0.0" // TODO: read from manifest
 
 	// Instantiate Package object and return it
 	p := Package{
