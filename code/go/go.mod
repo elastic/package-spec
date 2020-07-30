cd537d21beb0bcaaadeb0c4cba0e7ed4e75a03de
Shaunak Kashyap <ycombinator@gmail.com>
2020-07-30 06:45:24 -0700

    Adding validation error tests

--- code/go/go.mod
+++ code/go/go.mod
@@ -1,3 +1,5 @@
 module github.com/elastic/package-spec/code/go
 
 go 1.14
+
+require github.com/stretchr/testify v1.6.1
--- code/go/internal/validator/errors.go
+++ code/go/internal/validator/errors.go
@@ -1,11 +1,27 @@
 package validator
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // ValidationErrors is an Error that contains a iterable collection of validation error messages.
 type ValidationErrors []string
 
 func (ve ValidationErrors) Error() string {
-	return fmt.Sprintf("found %v validation errors", len(ve))
-}
+	if len(ve) == 0 {
+		return "found 0 validation errors"
+	}
 
+	var message strings.Builder
+	errorWord := "errors"
+	if len(ve) == 1 {
+		errorWord = "error"
+	}
+	fmt.Fprintf(&message, "found %v validation %v:\n", len(ve), errorWord)
+	for _, err := range ve {
+		fmt.Fprintf(&message, "\t%v\n", err)
+	}
+
+	return message.String()
+}
