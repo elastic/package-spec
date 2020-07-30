d7dcc6edcf8d8e5ad4d8e3b9cec58470fc48b962
Shaunak Kashyap <ycombinator@gmail.com>
2020-07-30 16:00:47 -0700

    Adding package tests

--- code/go/internal/validator/package.go
+++ code/go/internal/validator/package.go
@@ -1,20 +1,56 @@
 package validator
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/pkg/errors"
+)
+
 type Package struct {
 	SpecVersion string
 	RootPath    string
 }
 
 func NewPackage(pkgRootPath string) (*Package, error) {
-	// TODO: Check that package root path exists and is a folder
-	// TODO: Check that package root path contains manifest file
+	info, err := os.Stat(pkgRootPath)
+	if os.IsNotExist(err) {
+		return nil, errors.Wrapf(err, "no package found at path [%v]", pkgRootPath)
+	}
 
-	// Load package's manifest file and parse spec version
-	specVersion := "1.0.0" // TODO: read from manifest
+	if !info.IsDir() {
+		return nil, fmt.Errorf("no package folder found at path [%v]", pkgRootPath)
+	}
+
+	pkgManifestPath := path.Join(pkgRootPath, "manifest.yml")
+	info, err = os.Stat(pkgManifestPath)
+	if os.IsNotExist(err) {
+		return nil, errors.Wrapf(err, "no package manifest file found at path [%v]", pkgManifestPath)
+	}
+
+	data, err := ioutil.ReadFile(pkgManifestPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read package manifest file [%v]", pkgManifestPath)
+	}
+
+	var manifest struct {
+		SpecVersion string `yaml:"format_version"`
+	}
+	if err := yaml.Unmarshal(data, &manifest); err != nil {
+		return nil, errors.Wrapf(err, "could not parse package manifest file [%v]", pkgManifestPath)
+	}
+
+	if manifest.SpecVersion == "" {
+		return nil, fmt.Errorf("could not read specification version from package manifest file [%v]", pkgManifestPath)
+	}
 
 	// Instantiate Package object and return it
 	p := Package{
-		specVersion,
+		manifest.SpecVersion,
 		pkgRootPath,
 	}
 
