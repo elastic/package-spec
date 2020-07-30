8420edffc82af3851f094b35ff2480dcbd9af3b3
Shaunak Kashyap <ycombinator@gmail.com>
2020-07-30 12:52:36 -0700

    Add recipe

--- code/go/internal/validator/spec.go
+++ code/go/internal/validator/spec.go
@@ -14,5 +14,13 @@ func NewSpec(version string) (*Spec, error) {
 }
 
 func (s Spec) ValidatePackage(pkg Package) ValidationErrors {
+	// Stitch together specification YAML files into mega YAML specification
+	// Convert mega YAML non-JSON schema parts to JSON schema equivalents
+	// Convert mega YAML specification into mega JSON object (so we have a valid JSON schema)
+	// Stitch together package contents into mega JSON object
+	// Validate mega JSON object representing package against mega JSON schema
+	// Parse validation errors and make them friendlier so they make sense in the context of packages
+	// Perform additional non-trivial semantic validations
+	// Return validation errors
 	return nil
 }
