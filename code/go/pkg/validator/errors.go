fbb6b572de06eafa012f7398171a83577c5fb65a
Shaunak Kashyap <ycombinator@gmail.com>
2020-07-30 12:34:13 -0700

    Minor linting

--- code/go/pkg/validator/errors.go
+++ code/go/pkg/validator/errors.go
@@ -3,6 +3,6 @@ package validator
 import (
 	"github.com/elastic/package-spec/code/go/internal/validator"
 )
+
 // ValidationErrors is an Error that contains a iterable collection of validation error messages.
 type ValidationErrors validator.ValidationErrors
-
