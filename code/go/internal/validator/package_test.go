22e411e995eab4a381a54c2291b0a63a6bf9b7fd
Shaunak Kashyap <ycombinator@gmail.com>
2020-07-30 18:00:03 -0700

    Consolidating tests

--- code/go/internal/validator/package_test.go
+++ code/go/internal/validator/package_test.go
@@ -8,46 +8,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewPackageGood(t *testing.T) {
+func TestNewPackage(t *testing.T) {
+	tests := map[string]struct {
+		expectedErrContains string
+		expectedSpecVersion string
+	}{
+		"good": {
+			expectedSpecVersion: "1.0.4",
+		},
+		"non_existent": {
+			expectedErrContains: "no package found at",
+		},
+		"no_manifest": {
+			expectedErrContains: "no package manifest file found at",
+		},
+		"no_spec_version": {
+			expectedErrContains: "could not read specification version",
+		},
+	}
+
 	cwd, err := os.Getwd()
 	require.NoError(t, err)
 
-	pkgRootPath := path.Join(cwd, "test", "packages", "good")
-	pkg, err := NewPackage(pkgRootPath)
-	require.NoError(t, err)
-	require.Equal(t, "1.0.4", pkg.SpecVersion)
-	require.Equal(t, pkgRootPath, pkg.RootPath)
-}
-
-func TestNewPackageNonExistent(t *testing.T) {
-	cwd, err := os.Getwd()
-	require.NoError(t, err)
-
-	pkgRootPath := path.Join(cwd, "test", "packages", "nonexistent")
-	pkg, err := NewPackage(pkgRootPath)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "no package found at")
-	require.Nil(t, pkg)
-}
-
-func TestNewPackageNoManifest(t *testing.T) {
-	cwd, err := os.Getwd()
-	require.NoError(t, err)
-
-	pkgRootPath := path.Join(cwd, "test", "packages", "no_manifest")
-	pkg, err := NewPackage(pkgRootPath)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "no package manifest file found at")
-	require.Nil(t, pkg)
-}
-
-func TestNewPackageBadManifest(t *testing.T) {
-	cwd, err := os.Getwd()
-	require.NoError(t, err)
-
-	pkgRootPath := path.Join(cwd, "test", "packages", "bad_manifest")
-	pkg, err := NewPackage(pkgRootPath)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "could not read specification version")
-	require.Nil(t, pkg)
+	for pkgName, test := range tests {
+		pkgRootPath := path.Join(cwd, "test", "packages", pkgName)
+		pkg, err := NewPackage(pkgRootPath)
+		if test.expectedErrContains == "" {
+			require.NoError(t, err)
+			require.Equal(t, test.expectedSpecVersion, pkg.SpecVersion)
+			require.Equal(t, pkgRootPath, pkg.RootPath)
+		} else {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), test.expectedErrContains)
+			require.Nil(t, pkg)
+		}
+	}
 }
