37299a00184e68c6391f34a162d194d7717d1313
Shaunak Kashyap <ycombinator@gmail.com>
2020-08-06 11:44:32 -0700

    Refer to internal ValidationErrors type

--- code/go/pkg/validator/validator.go
+++ code/go/pkg/validator/validator.go
@@ -1,9 +1,13 @@
 package validator
 
+import (
+	"github.com/elastic/package-spec/code/go/internal/validator"
+)
+
 // ValidateFromPath validates a package located at the given path against the
 // appropriate specification and returns any errors.
 func ValidateFromPath(packageRootPath string) error {
 	// TODO: Noop for now. Implement actual validation.
-	var errs ValidationErrors
+	var errs validator.ValidationErrors
 	return errs
 }
