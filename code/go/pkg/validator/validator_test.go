6cc2335a4f89b87203e997c2f45680da6a73e9b9
mtojek <marcin.tojek@elastic.co>
2020-10-21 12:43:16 +0200

    Validate that dashboard IDs start with package names

--- code/go/pkg/validator/validator_test.go
+++ code/go/pkg/validator/validator_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/elastic/package-spec/code/go/internal/validator"
 )
 
-func TestValidate(t *testing.T) {
+func TestValidateFile(t *testing.T) {
 	tests := map[string]struct {
 		invalidPkgFilePath  string
 		expectedErrContains []string
@@ -18,15 +18,15 @@ func TestValidate(t *testing.T) {
 		"good":          {},
 		"deploy_docker": {},
 		"bad_deploy_variants": {
-			"_dev/deploy/variants.yml",
-			[]string{
+			invalidPkgFilePath: "_dev/deploy/variants.yml",
+			expectedErrContains: []string{
 				"field (root): default is required",
 				"field variants: Invalid type. Expected: object, given: array",
 			},
 		},
 		"missing_pipeline_dashes": {
-			"data_stream/foo/elasticsearch/ingest_pipeline/default.yml",
-			[]string{
+			invalidPkgFilePath: "data_stream/foo/elasticsearch/ingest_pipeline/default.yml",
+			expectedErrContains: []string{
 				"document dashes are required (start the document with '---')",
 			},
 		},
@@ -49,6 +49,37 @@ func TestValidate(t *testing.T) {
 					expectedErr := errPrefix + test.expectedErrContains[idx]
 					require.Contains(t, err.Error(), expectedErr)
 				}
+			}
+		})
+	}
+}
+
+func TestItemNotAllowed(t *testing.T) {
+	tests := map[string]struct {
+		itemFolder   string
+		invalidItems []string
+	}{
+		"wrong_dashboard_filename": {
+			itemFolder: "kibana/dashboard",
+			invalidItems: []string{
+				"b7e55b73-97cc-44fd-8555-d01b7e13e70d.json",
+				"bad-dashboard.json",
+			},
+		},
+	}
+
+	for pkgName, test := range tests {
+		t.Run(pkgName, func(t *testing.T) {
+			pkgRootPath := filepath.Join("..", "..", "internal", "validator", "test", "packages", pkgName)
+
+			errs := ValidateFromPath(pkgRootPath)
+			require.Error(t, errs)
+			require.Len(t, errs, len(test.invalidItems))
+			vErrs, ok := errs.(validator.ValidationErrors)
+			require.True(t, ok)
+			for idx, err := range vErrs {
+				expected := fmt.Sprintf("item [%s] is not allowed in folder [%s/%s]", test.invalidItems[idx], pkgRootPath, test.itemFolder)
+				require.Contains(t, err.Error(), expected)
 			}
 		})
 	}
