33b54fb5403b6cc36449dcba1825b5ed7d02a3e8
Marcin Tojek <mtojek@users.noreply.github.com>
2020-11-02 14:01:28 +0100

    Fix: invalid fields file regex (#76)
    * Fix: invalid fields file regex
    
    * Revert pattern change
    
    * Add start end line markers to spec files

--- code/go/internal/validator/folder_spec.go
+++ code/go/internal/validator/folder_spec.go
@@ -175,14 +175,14 @@ func (s *folderSpec) findItemSpec(packageName string, folderItemName string) (*f
 			return &itemSpec, nil
 		}
 		if itemSpec.Pattern != "" {
-			isMatch, err := regexp.MatchString("^" + strings.ReplaceAll(itemSpec.Pattern, "{PACKAGE_NAME}", packageName) + "$", folderItemName)
+			isMatch, err := regexp.MatchString(strings.ReplaceAll(itemSpec.Pattern, "{PACKAGE_NAME}", packageName), folderItemName)
 			if err != nil {
 				return nil, errors.Wrap(err, "invalid folder item spec pattern")
 			}
 			if isMatch {
 				var isForbidden bool
 				for _, forbidden := range itemSpec.ForbiddenPatterns {
-					isForbidden, err = regexp.MatchString("^" + forbidden + "$", folderItemName)
+					isForbidden, err = regexp.MatchString(forbidden, folderItemName)
 					if err != nil {
 						return nil, errors.Wrap(err, "invalid forbidden pattern for folder item")
 					}
