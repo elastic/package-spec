f057be5a84746469218d2033ca2501223e93395f
mtojek <marcin.tojek@elastic.co>
2022-05-23 14:18:28 +0200

    Log warning

--- code/go/internal/validator/folder_spec.go
+++ code/go/internal/validator/folder_spec.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io/fs"
 	"io/ioutil"
+	"log"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -88,6 +89,8 @@ func (s *folderSpec) validate(pkg *Package, path string) ve.ValidationErrors {
 	// Don't enable beta features for packages marked as GA.
 	if s.Release == "beta" && pkg.Version.Prerelease() == "" {
 		errs = append(errs, errors.Errorf("spec for [%s] defines beta features which can't be enabled for packages with a stable semantic version", pkg.Path(path)))
+	} else if s.Release == "beta" && pkg.Version.Prerelease() != "" {
+		log.Printf("Warning: package with non-stable semantic version and active beta features (enabled in [%s]) can't be released as stable version.q", pkg.Path(path))
 	}
 
 	for _, file := range files {
