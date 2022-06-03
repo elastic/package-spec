42b1361173a5c8d3d9539edb69135cdc4f28b943
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2022-06-03 13:32:36 +0200

    Don't check type for imported required fields (#347)

--- code/go/internal/validator/semantic/validate_required_fields.go
+++ code/go/internal/validator/semantic/validate_required_fields.go
@@ -46,9 +46,13 @@ func validateRequiredFields(fsys fspath.FS, requiredFields map[string]string) ve
 		if _, ok := foundFields[datastream]; !ok {
 			foundFields[datastream] = make(map[string]struct{})
 		}
-
 		foundFields[datastream][f.Name] = struct{}{}
-		if f.Type != expectedType {
+
+		// Check if type is the expected one, but only for fields what are
+		// not declared as external. External fields won't have a type in
+		// the definition.
+		// More info in https://github.com/elastic/elastic-package/issues/749
+		if f.External == "" && f.Type != expectedType {
 			return ve.ValidationErrors{errors.Errorf("expected type %q for required field %q, found %q in %q", expectedType, f.Name, f.Type, fieldsFile)}
 		}
 
