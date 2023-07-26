991ae115d7ac50175902072d2e3a4bb685f36c02
Bharat Pasupula <123897612+bhapas@users.noreply.github.com>
2023-07-26 17:54:50 +0200

    Add support for additional files for Terraform scenarios (#571)
    Allow to include yaml, json, conf and log files in the Terraform scenario definition
    so they can be used by Terraform resources or providers.
    
    Co-authored-by: Jaime Soriano Pastor <jaime.soriano@elastic.co>

--- test/packages/deploy_terraform/_dev/deploy/tf/main.tf
+++ test/packages/deploy_terraform/_dev/deploy/tf/main.tf
@@ -1,1 +1,10 @@
 # A Terraform file
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "new_object_key"
+  source = "./logs/example.log"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "test-terraform-deploy-bucket"
+}
