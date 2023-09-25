06854fef97e8783e7345edb8a3e2c918cd28fee3
Drew Tate <drewctate@gmail.com>
2023-09-25 08:28:14 -0600

    support by-reference visualizations

--- code/go/internal/validator/semantic/validate_kibana_no_legacy_visualizations.go
+++ code/go/internal/validator/semantic/validate_kibana_no_legacy_visualizations.go
@@ -20,33 +20,63 @@ import (
 func ValidateKibanaNoLegacyVisualizations(fsys fspath.FS) ve.ValidationErrors {
 	var errs ve.ValidationErrors
 
-	filePaths := path.Join("kibana", "dashboard", "*.json")
-	dashboardFiles, err := pkgpath.Files(fsys, filePaths)
+	// Collect by-reference visualizations for reference later.
+	// Note: this does not include Lens, Maps, or Discover. That's okay for this rule because none of those are legacy
+	visFilePaths := path.Join("kibana", "visualization", "*.json")
+	visFiles, _ := pkgpath.Files(fsys, visFilePaths)
+	pkgByRefVisualizations := make(map[string]kbncontent.VisDesc)
+
+	for _, file := range visFiles {
+		if visJSON, err := file.Values("$"); err == nil {
+			if desc, err := kbncontent.DescribeVisualizationSavedObject(visJSON.(map[string]interface{})); err == nil {
+				pkgByRefVisualizations[strings.TrimSuffix(file.Name(), ".json")] = desc
+			}
+		}
+	}
+
+	dashboardFilePaths := path.Join("kibana", "dashboard", "*.json")
+	dashboardFiles, err := pkgpath.Files(fsys, dashboardFilePaths)
 	if err != nil {
 		errs = append(errs, fmt.Errorf("error finding Kibana dashboard files: %w", err))
 		return errs
 	}
+
 	for _, file := range dashboardFiles {
 		dashboardJSON, _ := file.Values("$")
-		dashboardTitle, _ := kbncontent.GetDashboardTitle(dashboardJSON)
 		panelsJSON, _ := file.Values("$.attributes.panelsJSON")
 		visualizations, _ := kbncontent.DescribeByValueDashboardPanels(panelsJSON)
 
-		var buf strings.Builder
-		fmt.Fprint(&buf, "your package contains legacy visualizations:\n\n")
-		tableWriter := tabwriter.NewWriter(&buf, 0, 0, 1, ' ', tabwriter.Debug)
-		fmt.Fprintln(tableWriter, "\tDashboard title\tVisualization title\tVisualization editor\tVisualization type\t")
-		fmt.Fprintln(tableWriter, "\t\t\t\t\t")
-		hasLegacy := false
-		for _, vis := range visualizations {
-			if vis.IsLegacy {
-				hasLegacy = true
-				fmt.Fprintf(tableWriter, "\t\"%s\"\t\"%s\"\t%s\t%s\t\n", dashboardTitle, vis.Title, vis.Editor, vis.SemanticType)
+		var byRefVisualizations []kbncontent.VisDesc
+		ids, _ := kbncontent.GetByReferencePanelIDs(dashboardJSON)
+		for _, id := range ids {
+			if vis, ok := pkgByRefVisualizations[id]; ok {
+				byRefVisualizations = append(byRefVisualizations, vis)
 			}
 		}
-		tableWriter.Flush()
 
-		if hasLegacy {
+		visualizations = append(visualizations, byRefVisualizations...)
+
+		var legacyVisualizations []kbncontent.VisDesc
+		for _, desc := range visualizations {
+			if desc.IsLegacy {
+				legacyVisualizations = append(legacyVisualizations, desc)
+			}
+		}
+
+		if len(legacyVisualizations) > 0 {
+			dashboardTitle, _ := kbncontent.GetDashboardTitle(dashboardJSON)
+			var buf strings.Builder
+			fmt.Fprint(&buf, "your package contains legacy visualizations:\n\n")
+
+			tableWriter := tabwriter.NewWriter(&buf, 0, 0, 1, ' ', tabwriter.Debug)
+			fmt.Fprintln(tableWriter, "\tDashboard title\tVisualization title\tVisualization editor\tVisualization type\t")
+			fmt.Fprintln(tableWriter, "\t\t\t\t\t")
+
+			for _, vis := range legacyVisualizations {
+				fmt.Fprintf(tableWriter, "\t\"%s\"\t\"%s\"\t%s\t%s\t\n", dashboardTitle, vis.Title, vis.Editor, vis.SemanticType)
+			}
+			tableWriter.Flush()
+
 			errs = append(errs, fmt.Errorf("%s", buf.String()))
 		}
 	}
