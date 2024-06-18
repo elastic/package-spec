1d0c0a3305f0d0479cfb0b8829edb05b259eac15
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-06-18 11:47:33 +0200

    Set 3.1.5 for 8.14.* stack versions

--- .buildkite/pipeline.trigger.compliance.tests.sh
+++ .buildkite/pipeline.trigger.compliance.tests.sh
@@ -27,8 +27,8 @@ EOF
 
 # Generate each test we want to do.
 compliance_test 8.15.0-SNAPSHOT 3.2.0
-compliance_test 8.14.1-SNAPSHOT 3.2.0
-compliance_test 8.14.0 3.2.0
+compliance_test 8.14.1-SNAPSHOT 3.1.5
+compliance_test 8.14.0 3.1.5
 compliance_test 8.9.0 2.7.0
 
 # Annotate junit results.
