074462433253257e5295bec6be4842457ec33e1b
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2024-07-03 13:07:55 +0200

    Prepare release 3.2.1

--- .buildkite/pipeline.trigger.compliance.tests.sh
+++ .buildkite/pipeline.trigger.compliance.tests.sh
@@ -28,7 +28,7 @@ steps:
 EOF
 
 # Generate each test we want to do.
-compliance_test 8.15.0-SNAPSHOT 3.2.0
+compliance_test 8.15.0-SNAPSHOT 3.2.1
 compliance_test 8.14.0 3.1.5
 compliance_test 8.9.0 2.7.0
 
