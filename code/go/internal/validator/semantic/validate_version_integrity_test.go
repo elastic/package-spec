41771eb7db5fb180f21c3b367f8046f43692e6d9
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-10-01 19:28:08 +0200

    Create new test file

--- code/go/internal/validator/semantic/validate_version_integrity_test.go
+++ code/go/internal/validator/semantic/validate_version_integrity_test.go
@@ -5,12 +5,9 @@
 package semantic
 
 import (
-	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/elastic/package-spec/v3/code/go/pkg/specerrors"
 )
 
 func TestChangelogUniqueVersions(t *testing.T) {
@@ -148,100 +145,6 @@ func TestChangelogLatestVersionIsGreaterThanOthers(t *testing.T) {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
-			}
-		})
-	}
-}
-
-func TestValidateGithubLink(t *testing.T) {
-	var tests = []struct {
-		link        string
-		expectedErr error
-	}{
-		{
-			"https://github.com/elastic/integrations/pull/2897",
-			nil,
-		},
-		{
-			"https://github.com/elastic/integrations/pull/abcd",
-			errGithubIssue,
-		},
-		{
-			"https://github.com/elastic/integrations/pull/0",
-			errGithubIssue,
-		},
-		{
-			"https://github.com/elastic/integrations/pull",
-			errGithubIssue,
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.link, func(t *testing.T) {
-			linkURL, _ := url.Parse(test.link)
-			err := validateGithubLink(linkURL)
-			if err != nil {
-				assert.ErrorIs(t, err, test.expectedErr)
-			} else {
-				assert.Equal(t, err, test.expectedErr)
-			}
-		})
-	}
-}
-
-func TestEnsureLinksAreValid(t *testing.T) {
-	var githubError = specerrors.NewStructuredError(errGithubIssue, specerrors.UnassignedCode)
-
-	var tests = []struct {
-		name   string
-		links  []string
-		errors specerrors.ValidationErrors
-	}{
-		{
-			"AllValidLinks",
-			[]string{
-				"https://github.com/elastic/integrations/pull/2897",
-				"https://github.com/elastic/integrations/pull/1001",
-				"https://github.com/elastic/integrations/pull/1",
-			},
-			nil,
-		},
-		{
-			"AllInvalidLinks",
-			[]string{
-				"https://github.com/elastic/integrations/pull/abcd",
-				"https://github.com/elastic/integrations/pull",
-			},
-			specerrors.ValidationErrors{
-				githubError,
-				githubError,
-			},
-		},
-		{
-			"SomeInvalidLinks",
-			[]string{
-				"https://github.com/elastic/integrations/pull/1234",
-				"https://github.com/elastic/integrations/pull",
-			},
-			specerrors.ValidationErrors{
-				githubError,
-			},
-		},
-		{
-			"IgnoreCasesOtherThanGithubDotCom",
-			[]string{
-				"https://gitlab.com/elastic/integrations/pull/abcd",
-				"https://zzz.com/elastic/integrations/pull/1234",
-			},
-			nil,
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			errs := ensureLinksAreValid(test.links)
-			if test.errors == nil {
-				assert.Equal(t, test.errors, errs)
-			} else {
-				assert.Equal(t, len(errs), len(test.errors))
 			}
 		})
 	}
