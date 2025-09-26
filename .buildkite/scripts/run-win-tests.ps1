0220bd498acabc98a17f9c3cf183f158c9c2bc88
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-09-26 13:01:34 +0200

    Download modules

--- .buildkite/scripts/run-win-tests.ps1
+++ .buildkite/scripts/run-win-tests.ps1
@@ -2,13 +2,13 @@ $ErrorActionPreference = "Stop" # set -e
 # Forcing to checkout again all the files with a correct autocrlf.
 # Doing this here because we cannot set git clone options before.
 function fixCRLF {
-    Write-Host "-- Fixing CRLF in git checkout --"
+    Write-Host "--- Fixing CRLF in git checkout"
     git config core.autocrlf input
     git rm --quiet --cached -r .
     git reset --quiet --hard
 }
 function withGolang($version) {
-    Write-Host "-- Install golang $version --"
+    Write-Host "--- Install golang $version"
     choco install -y golang --version $version
     $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
     Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -29,6 +29,10 @@ fixCRLF
 withGolang $env:GO_VERSION
 installGoDependencies
 
+echo "--- Downloading Go modules"
+go mod download -x
+
+echo "--- Running unit tests"
 $ErrorActionPreference = "Continue" # set +e
 
 gotestsum --format testname --junitfile junit-win-report.xml -- -v ./code/go/...
