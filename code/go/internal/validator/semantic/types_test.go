41ad10fb101305c312eb31ace1b84aa9a0226ac5
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-11-27 10:44:41 +0100

    Remove unnecessary struct type in arrays

--- code/go/internal/validator/semantic/types_test.go
+++ code/go/internal/validator/semantic/types_test.go
@@ -25,88 +25,88 @@ func TestListFieldsFiles(t *testing.T) {
 		{
 			pkgName: "good_v2",
 			expected: []fieldFileMetadata{
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/foo/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/foo/fields/base-fields.yml"),
 					dataStream:   "foo",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/foo/fields/external-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/foo/fields/external-fields.yml"),
 					dataStream:   "foo",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/foo/fields/some_fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/foo/fields/some_fields.yml"),
 					dataStream:   "foo",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/hidden_data_stream/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/hidden_data_stream/fields/base-fields.yml"),
 					dataStream:   "hidden_data_stream",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/hidden_data_stream/fields/some_fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/hidden_data_stream/fields/some_fields.yml"),
 					dataStream:   "hidden_data_stream",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/ilm_policy/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/ilm_policy/fields/base-fields.yml"),
 					dataStream:   "ilm_policy",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/ilm_policy/fields/some_fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/ilm_policy/fields/some_fields.yml"),
 					dataStream:   "ilm_policy",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/k8s_data_stream/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/k8s_data_stream/fields/base-fields.yml"),
 					dataStream:   "k8s_data_stream",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/k8s_data_stream_no_definitions/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/k8s_data_stream_no_definitions/fields/base-fields.yml"),
 					dataStream:   "k8s_data_stream_no_definitions",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/pe/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/pe/fields/base-fields.yml"),
 					dataStream:   "pe",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/pe/fields/some_fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/pe/fields/some_fields.yml"),
 					dataStream:   "pe",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/routing_rules/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/routing_rules/fields/base-fields.yml"),
 					dataStream:   "routing_rules",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/skipped_tests/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/skipped_tests/fields/base-fields.yml"),
 					dataStream:   "skipped_tests",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "data_stream/skipped_tests/fields/some_fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/data_stream/skipped_tests/fields/some_fields.yml"),
 					dataStream:   "skipped_tests",
 				},
 				// transforms
-				fieldFileMetadata{
+				{
 					filePath:     "elasticsearch/transform/metadata_current/fields/fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/elasticsearch/transform/metadata_current/fields/fields.yml"),
 					transform:    "metadata_current",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "elasticsearch/transform/metadata_united/fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/elasticsearch/transform/metadata_united/fields/base-fields.yml"),
 					transform:    "metadata_united",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "elasticsearch/transform/metadata_united/fields/fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_v2/elasticsearch/transform/metadata_united/fields/fields.yml"),
 					transform:    "metadata_united",
@@ -116,12 +116,12 @@ func TestListFieldsFiles(t *testing.T) {
 		{
 			pkgName: "good_input",
 			expected: []fieldFileMetadata{
-				fieldFileMetadata{
+				{
 					filePath:     "fields/base-fields.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_input/fields/base-fields.yml"),
 					dataStream:   "",
 				},
-				fieldFileMetadata{
+				{
 					filePath:     "fields/input.yml",
 					fullFilePath: filepath.FromSlash("../../../../../test/packages/good_input/fields/input.yml"),
 					dataStream:   "",
