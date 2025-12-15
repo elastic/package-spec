bea65556dec28837f5b53fa615dd8866df5fb5d2
Tere <teresa.romero@elastic.co>
2025-12-15 12:24:16 +0100

    Handlebars template static validation and documentation (#1030)
    Includes a validation rule for verifying the static .hbs files used at input tempate and stream template are correct.
    Also adds more information on the spec regarding the format of these templates and the helpers that are available at fleet when rendering them.
    Linked file are also verified .hbs.link
    
    The validation is done over the static syntax of handlebars, does not validate the render of the template taking into account the values of the values.

---
+++ code/go/internal/validator/semantic/validate_hbs_templates.go
@@ -0,0 +1,117 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package semantic
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path"
+
+	"github.com/aymerick/raymond"
+
+	"github.com/elastic/package-spec/v3/code/go/internal/fspath"
+	"github.com/elastic/package-spec/v3/code/go/internal/linkedfiles"
+	"github.com/elastic/package-spec/v3/code/go/pkg/specerrors"
+)
+
+var (
+	errInvalidHandlebarsTemplate = errors.New("invalid handlebars template")
+)
+
+// ValidateStaticHandlebarsFiles validates all Handlebars (.hbs) files in the package filesystem.
+// It returns a list of validation errors if any Handlebars files are invalid.
+// hbs are located in both the package root and data stream directories under the agent folder.
+func ValidateStaticHandlebarsFiles(fsys fspath.FS) specerrors.ValidationErrors {
+	var errs specerrors.ValidationErrors
+
+	// template files are placed at /agent/input directory or
+	// at the datastream /agent/stream directory
+	inputDir := path.Join("agent", "input")
+	if inputErrs := validateTemplateDir(fsys, inputDir); inputErrs != nil {
+		errs = append(errs, inputErrs...)
+	}
+
+	datastreamEntries, err := fs.ReadDir(fsys, "data_stream")
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return specerrors.ValidationErrors{
+			specerrors.NewStructuredErrorf("error reading data_stream directory: %w", err),
+		}
+	}
+	for _, dsEntry := range datastreamEntries {
+		if !dsEntry.IsDir() {
+			continue
+		}
+		streamDir := path.Join("data_stream", dsEntry.Name(), "agent", "stream")
+		dsErrs := validateTemplateDir(fsys, streamDir)
+		if dsErrs != nil {
+			errs = append(errs, dsErrs...)
+		}
+	}
+
+	return errs
+}
+
+// validateTemplateDir validates all Handlebars files in the given directory.
+func validateTemplateDir(fsys fspath.FS, dir string) specerrors.ValidationErrors {
+	entries, err := fs.ReadDir(fsys, dir)
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return specerrors.ValidationErrors{
+			specerrors.NewStructuredErrorf("error trying to read :%s", dir),
+		}
+	}
+	var errs specerrors.ValidationErrors
+	for _, entry := range entries {
+		if path.Ext(entry.Name()) == ".hbs" {
+			err := validateStaticHandlebarsEntry(fsys, dir, entry.Name())
+			if err != nil {
+				errs = append(errs, specerrors.NewStructuredErrorf("%w: error validating %s: %w", errInvalidHandlebarsTemplate, path.Join(dir, entry.Name()), err))
+			}
+			continue
+		}
+		if path.Ext(entry.Name()) == ".link" {
+			linkFilePath := path.Join(dir, entry.Name())
+			linkFile, err := linkedfiles.NewLinkedFile(fsys.Path(linkFilePath))
+			if err != nil {
+				errs = append(errs, specerrors.NewStructuredErrorf("error reading linked file %s: %w", linkFilePath, err))
+				continue
+			}
+			err = validateStaticHandlebarsEntry(fsys, dir, linkFile.IncludedFilePath)
+			if err != nil {
+				errs = append(errs, specerrors.NewStructuredErrorf("%w: error validating %s: %w", errInvalidHandlebarsTemplate, path.Join(dir, linkFile.IncludedFilePath), err))
+			}
+		}
+	}
+	return errs
+}
+
+// validateStaticHandlebarsEntry validates a single Handlebars file located at filePath.
+// it parses the file using the raymond library to check for syntax errors.
+func validateStaticHandlebarsEntry(fsys fspath.FS, dir, entryName string) error {
+	if entryName == "" {
+		return nil
+	}
+
+	var content []byte
+	var err error
+
+	// First try to read from filesystem (works for regular files and files within zip)
+	filePath := path.Join(dir, entryName)
+	if content, err = fs.ReadFile(fsys, filePath); err != nil {
+		if !errors.Is(err, fs.ErrInvalid) {
+			return err
+		}
+		// If fs.ReadFile fails (likely due to linked file path outside filesystem boundary),
+		// fall back to absolute path approach like linkedfiles.FS does
+		absolutePath := fsys.Path(filePath)
+		if content, err = os.ReadFile(absolutePath); err != nil {
+			return err
+		}
+	}
+
+	// Parse from content string instead of file path
+	_, err = raymond.Parse(string(content))
+	return err
+}
--- compliance/go.mod
+++ compliance/go.mod
@@ -110,6 +110,7 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magefile/mage v1.15.0 // indirect
+	github.com/mailgun/raymond/v2 v2.0.48 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -145,6 +146,7 @@ require (
 	github.com/shirou/gopsutil/v3 v3.24.5 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.7.0 // indirect
 	github.com/spf13/cobra v1.10.1 // indirect
