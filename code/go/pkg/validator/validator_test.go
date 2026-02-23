68b63d2293bc6e5cb54e9da94d9965f9137b6438
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2026-02-23 10:59:00 +0100

    Allow to use otelcol inputs in integration packages (#1091)
    Allow to use otelcol inputs and dynamic_signal_types in integration
    packages starting on 3.6.0.
    
    otelcol was supported for input packages since 3.5.0.

--- code/go/pkg/validator/validator_test.go
+++ code/go/pkg/validator/validator_test.go
@@ -393,7 +393,18 @@ func TestValidateFile(t *testing.T) {
 			"manifest.yml",
 			[]string{
 				"field policy_templates.0: Additional property dynamic_signal_types is not allowed",
-				"policy template \"sample\": dynamic_signal_types is only allowed for input type packages",
+			},
+		},
+		"bad_integration_dynamic_signal_types_non_otel": {
+			"manifest.yml",
+			[]string{
+				"policy template \"sample\": input type \"logfile\": dynamic_signal_types is only allowed when input is 'otelcol'",
+			},
+		},
+		"bad_integration_otel_old_version": {
+			"manifest.yml",
+			[]string{
+				"field policy_templates.0.inputs.0.type: Must not be present",
 			},
 		},
 		"bad_input_dynamic_signal_types_old_version": {
