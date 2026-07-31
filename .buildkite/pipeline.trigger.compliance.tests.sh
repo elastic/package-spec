be7813d9e6016ad1f0c0d36b74432af59bde07a8
Tere <teresa.romero@elastic.co>
2026-07-31 12:08:56 +0200

    Update compliance test version for 9.5.0 in pipeline script
    - Changed compliance test version from 9.5.0 to 9.5.0-SNAPSHOT to align with the latest snapshot release.

--- .buildkite/pipeline.trigger.compliance.tests.sh
+++ .buildkite/pipeline.trigger.compliance.tests.sh
@@ -29,7 +29,7 @@ EOF
 
 # Generate each test we want to do.
 compliance_test 9.6.0-SNAPSHOT 3.6.6
-compliance_test 9.5.0 3.6.6
+compliance_test 9.5.0-SNAPSHOT 3.6.6
 compliance_test 9.4.4 3.6.6
 compliance_test 8.19.3 3.4.2
 compliance_test 9.0.6 3.3.5
